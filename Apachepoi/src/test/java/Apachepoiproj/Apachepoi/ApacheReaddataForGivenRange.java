4ddd88b346ed20f82a2e3741ed92a959ae6fca2e
x <x@DESKTOP-P3D8FQC>
2023-09-02 20:57:55 +0530

    Push first time

---
+++ Apachepoi/src/test/java/Apachepoiproj/Apachepoi/ApacheReaddataForGivenRange.java
@@ -0,0 +1,54 @@
+package Apachepoiproj.Apachepoi;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Scanner;
+
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class ApacheReaddataForGivenRange {
+	public void ReadDataForGivenRowRange(int startrow, int endrow) throws IOException
+	{
+		File f=new File("../Apachepoi/apchedeom.xlsx");
+		FileInputStream fis=new FileInputStream(f);
+		XSSFWorkbook xw=new XSSFWorkbook(fis);
+		XSSFSheet xs=xw.getSheetAt(0);
+		int a=xs.getPhysicalNumberOfRows();
+		
+		for(int i=0;i<a;i++)
+		{
+			XSSFRow xr=xs.getRow(i);
+			int b=xr.getPhysicalNumberOfCells();
+			for(int j=0;j<b;j++)
+			{
+				if(i<=endrow)
+				{
+					XSSFCell xc=xr.getCell(j);
+					System.out.println( xc.getStringCellValue());
+				
+				}
+				}
+			
+			
+		}		
+		
+	}
+
+	public static void main(String[] args) throws IOException 
+	{
+        Scanner s = new Scanner(System.in);
+		System.out.println("Please enter start row no");
+		int c=s.nextInt();
+		System.out.println("Please enter end row no");
+		int d=s.nextInt();
+		ApacheReaddataForGivenRange ad=new ApacheReaddataForGivenRange();
+		ad.ReadDataForGivenRowRange(c,d);
+
+	}
+}
+
+
